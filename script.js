f622e1c594bf32cfd4e7624aa3005507ca48130b
AprilOLoughlin1 <apriljaneoloughlin@gmail.com>
2025-08-30 04:30:17 +1000

    add theme toggle button and js

---
+++ script.js
@@ -0,0 +1,14 @@
+function switchTheme() {
+    const body = document.body;
+    const toggleButton = document.querySelector('.toggle-button');
+    const themeName = document.getElementById('theme-name');
+
+    body.classList.toggle('dark-mode');
+    toggleButton.classList.toggle('active');
+
+    if (body.classList.contains('dark-mode')) {
+        themeName.textContent = 'Dark';
+    } else {
+        themeName.textContent = 'Light';
+    }
+}
